719488329d78ad614dd0153dcc8434e6a1acf193
Jake Kirk <jake.kirk@methods.co.uk>
2023-07-28 17:15:10 +0100

    Implemented getting queries from graphql

--- index.js
+++ index.js
@@ -1,15 +1,49 @@
+const { gql, GraphQLClient } = require("graphql-request");
+const dotenv = require('dotenv');
 const express = require('express');
 const nunjucks = require('nunjucks');
 const app = express();
-const port = 3000;
+
+dotenv.config();
+
+const port = process.env.PORT ?? 3000;
+
+const client = new GraphQLClient(process.env.GRAPHQL_ENDPOINT, {
+  headers: {
+    authorization: `bearer ${process.env.GRAPHQL_TOKEN}`,
+  },
+});
+console.log(process.env.GRAPHQL_ENDPOINT)
 
 nunjucks.configure('views', {
   autoescape: true,
   express: app,
 });
 
-app.get('/', (req, res) => {
+const query = gql`
+  query {
+    questions {
+      data {
+        attributes {
+          question
+        }
+      }
+    }
+  }
+`;
+
+app.get('/', async(req, res) => {
+  const data = await client.request(query);
+
+  const questions = data.questions.data.map(questionData => ({
+    questionText: questionData.attributes.question,
+  }));
+  
+  console.log(questions);
+
+  //console.log(JSON.stringify(data, null, 2))
   res.render('index.njk', {
+    questions,
     someNumber: 42,
     someItems: [   
       {   
@@ -24,4 +58,5 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
+  console.log(`view here: http://localhost:${port}`);
 });
