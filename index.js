af30e79772999656f51588e59c9482b71e421789
Jake Kirk <jake.kirk@methods.co.uk>
2023-07-28 10:03:28 +0100

    Nunjucks setup

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const nunjucks = require('nunjucks');
 const app = express();
 const port = 3000;
 
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app,
+});
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.render('index.njk', {
+    someNumber: 42,
+    someItems: [   
+      {   
+        title: 'First item',  
+      },  
+      {  
+        title: 'Second item', 
+      },  
+    ],
+  });
 });
 
 app.listen(port, () => {
