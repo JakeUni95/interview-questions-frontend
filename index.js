aec6aecbd8f245c3b7f775633f1f9c20f2d9e592
Jake Kirk <jake.kirk@methods.co.uk>
2023-08-15 17:25:35 +0100

    transforming question data

--- index.js
+++ index.js
@@ -36,36 +36,42 @@ nunjucks.configure([
 
 app.set('view engine', 'njk');
 
-const queryQuestion = gql`
-  query {
-    questions {
-      data {
-        attributes {
-          question
+const queryGetQuestionData = gql`
+  questions {
+    data {
+      attributes {
+        question
+        skills {
+          data {
+            attributes {
+              skillName
+            }
+          } 
+        }  
+      }
+    }
+  }
+}
+`;
+
+
+const queryGetSkillData = gql` {
+  skills {
+    data {
+      attributes {
+        skillName
+        skillCategories {
+          data {
+            attributes {
+              skillCategoryName
+              }
+            }
+          }
         }
       }
     }
   }
 `;
-
-
- const queryGetSkillData = gql` {
-    skills{
-      data {
-        attributes {
-          skillName
-          skillCategories {
-            data {
-              attributes {
-                skillCategoryName
-              }
-            }
-          }
-       }
-      }
-     }
-    }
-    `;
 
 app.get('/', async(req, res) => {
   const data = await client.request(queryGetSkillData)
@@ -105,22 +111,172 @@ app.get('/', async(req, res) => {
 });
 
 app.get('/question', async(req, res) => {
-  const data = await client.request(queryQuestion);
+  const data = await client.request();
   
   const questions = data.questions.data.map(questionData => ({
     questionText: questionData.attributes.question,
   }));
+  console.log(questions);
     res.render('question.njk', {
-      questions: questions
+      questions: questions,
   });
 });
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
   console.log(`view here: http://localhost:${port}`);
 });
 
-
-
-
+let a = {
+  "data": {
+    "questions": {
+      "data": [
+        {
+          "attributes": {
+            "question": "Is Java a pure object-oriented programming language?",
+            "skills": {
+              "data": [
+                {
+                  "attributes": {
+                    "skillName": "Java"
+                  }
+                }
+              ]
+            }
+          }
+        },
+        {
+          "attributes": {
+            "question": "In C# how could you sort the elements of an array in descending order?",
+            "skills": {
+              "data": [
+                {
+                  "attributes": {
+                    "skillName": "C#"
+                  }
+                }
+              ]
+            }
+          }
+        },
+        {
+          "attributes": {
+            "question": "If one member of your team has a different point of view about what direction a project should be heading, how do you rectify this?",
+            "skills": {
+              "data": [
+                {
+                  "attributes": {
+                    "skillName": "Working in a team"
+                  }
+                }
+              ]
+            }
+          }
+        },
+        {
+          "attributes": {
+            "question": "What is a class?",
+            "skills": {
+              "data": [
+                {
+                  "attributes": {
+                    "skillName": "Java"
+                  }
+                },
+                {
+                  "attributes": {
+                    "skillName": "C#"
+                  }
+                }
+              ]
+            }
+          }
+        },
+        {
+          "attributes": {
+            "question": "What are the three models for cloud deployment?",
+            "skills": {
+              "data": [
+                {
+                  "attributes": {
+                    "skillName": "Azure"
+                  }
+                },
+                {
+                  "attributes": {
+                    "skillName": "AWS"
+                  }
+                }
+              ]
+            }
+          }
+        },
+        {
+          "attributes": {
+            "question": "What is AWS?",
+            "skills": {
+              "data": [
+                {
+                  "attributes": {
+                    "skillName": "AWS"
+                  }
+                }
+              ]
+            }
+          }
+        },
+        {
+          "attributes": {
+            "question": "How do you handle unexpected challenges or roadblocks when working independently?",
+            "skills": {
+              "data": [
+                {
+                  "attributes": {
+                    "skillName": "Working alone"
+                  }
+                }
+              ]
+            }
+          }
+        }
+      ]
+    }
+  }
+}
   
+const rawQuestions = a.data.questions.data;
+//console.log(rawQuestions);
+
+const allQuestions = rawQuestions.map(question => ({
+  question: question.attributes.question,
+  skill: question.attributes.skills.data.map(skill => skill.attributes.skillName)
+}));
+
+//console.log(allQuestions)
+
+const questionBySkillsMapping = allQuestions.reduce((groups, question) => {
+  if (!groups[question.skill]) {
+    groups[question.skill] = {
+      skills: [],
+      question: [],
+    };
+  }
+  groups[question.skill].skills.push(question.skill)
+  groups[question.skill].question.push(question)
+ return groups;
+}, {});
+console.log(questionBySkillsMapping);
+console.log(JSON.stringify(questionBySkillsMapping, null, 2));
+
+//console.log(Object.values(questionBySkillsMapping));
+// const skillsByCategoryMapping = allSkills.reduce((groups, skill) => {
+//   if (!groups[skill.category]) {
+//       groups[skill.category] = {
+//         title: skill.category,
+//         skills: [],
+//       };
+//     }
+//     groups[skill.category].skills.push(skill);
+//     return groups;
+//     }, {});
