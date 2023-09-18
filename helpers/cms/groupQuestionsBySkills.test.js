9a21c324566f5a83e490adecb25eb90d4f7a0037
Jake Kirk <jake.kirk@methods.co.uk>
2023-09-18 14:23:00 +0100

    Using the it.each function for readability

--- helpers/cms/groupQuestionsBySkills.test.js
+++ helpers/cms/groupQuestionsBySkills.test.js
@@ -1,72 +1,51 @@
 const groupQuestionsBySkills = require('./groupQuestionsBySkills');
 
+const javaQuestionWithSkill = {
+  question: 'Is Java a pure object-oriented programming language?',
+  whyDoWeAskThis: 'We want to know if the interviewee knows what makes a language object-oriented.',
+  guidanceAnswer: 'Java is not a pure object-oriented language as it uses primitive types such as byte, boolean, char, short, int, float, long and double.',
+  skill: 'Java',
+}
+
+const javaQuestionWithoutSkill = {
+  question: 'Is Java a pure object-oriented programming language?',
+  whyDoWeAskThis: 'We want to know if the interviewee knows what makes a language object-oriented.',
+  guidanceAnswer: 'Java is not a pure object-oriented language as it uses primitive types such as byte, boolean, char, short, int, float, long and double.',
+}
+
+const classQuestionWithSkill = {
+  question: 'What is a class?',
+  whyDoWeAskThis: 'To see if they have an understanding of classes.',
+  guidanceAnswer: 'A class is like a blueprint for creating objects.',
+  skill: 'Java',
+}
+
+const classQuestionWithoutSkill = {
+  question: 'What is a class?',
+  whyDoWeAskThis: 'To see if they have an understanding of classes.',
+  guidanceAnswer: 'A class is like a blueprint for creating objects.',
+}
+
+
 describe('groupQuestionsBySkills', () => {
-
-  it('It should group questions by their associated skills', () => {
-
-    const emptyInput = [];
-    const emptyOutput = {};
-
-    const noQuestions = [{
-      skill: 'Java'
-    }];
-
-    const noQuestionOutput = {
-      'Java': [{
-      }]
-    };
-
-    const oneQuestionOneSkill = [{
-      question: 'Is Java a pure object-oriented programming language?',
-      whyDoWeAskThis: 'We want to know if the interviewee knows what makes a language object-oriented.',
-      guidanceAnswer: 'Java is not a pure object-oriented language as it uses primitive types such as byte, boolean, char, short, int, float, long and double.',
-      skill: 'Java',
-    }];
-
-    const oneQuestionOneSkillOutput = {
-      'Java': [{
-        question: 'Is Java a pure object-oriented programming language?',
-        whyDoWeAskThis: 'We want to know if the interviewee knows what makes a language object-oriented.',
-        guidanceAnswer: 'Java is not a pure object-oriented language as it uses primitive types such as byte, boolean, char, short, int, float, long and double.',
-      }]
-    };
-
-    const TwoQuestionsOneSkill = [{
-      question: 'What is a class?',
-      whyDoWeAskThis: 'To see if they have an understanding of classes.',
-      guidanceAnswer: 'A class is like a blueprint for creating objects.',
-      skill: 'Java',
-    },
-    {
-      question: 'Is Java a pure object-oriented programming language?',
-      whyDoWeAskThis: 'We want to know if the interviewee knows what makes a language object-oriented.',
-      guidanceAnswer: 'Java is not a pure object-oriented language as it uses primitive types such as byte, boolean, char, short, int, float, long and double.',
-      skill: 'Java',
-    }];
-
-    const TwoQuestionsOneSkillOutput = {
-      'Java': [{
-        question: 'What is a class?',
-        whyDoWeAskThis: 'To see if they have an understanding of classes.',
-        guidanceAnswer: 'A class is like a blueprint for creating objects.',
-      },
-      {
-        question: 'Is Java a pure object-oriented programming language?',
-        whyDoWeAskThis: 'We want to know if the interviewee knows what makes a language object-oriented.',
-        guidanceAnswer: 'Java is not a pure object-oriented language as it uses primitive types such as byte, boolean, char, short, int, float, long and double.',
-      }]
-    };
-
-    const emptyInputResult = groupQuestionsBySkills(emptyInput);
-    expect(emptyInputResult).toEqual(emptyOutput);
-
-    const noQuestionsResult = groupQuestionsBySkills(noQuestions);
-    expect(noQuestionsResult).toEqual(noQuestionOutput);
-
-    const oneQuestionOneSkillResult = groupQuestionsBySkills(oneQuestionOneSkill);
-    expect(oneQuestionOneSkillResult).toEqual(oneQuestionOneSkillOutput);
-
-    const TwoQuestionsOneSkillResult = groupQuestionsBySkills(TwoQuestionsOneSkill);
-    expect(TwoQuestionsOneSkillResult).toEqual(TwoQuestionsOneSkillOutput);
-  })
-})
+  it.each([
+    [
+      [], 
+      {}
+    ],
+    [
+      [{skill: 'Java'}],
+      { 'Java': [{}]}
+    ],
+    [
+      [javaQuestionWithSkill], 
+      {'Java': [javaQuestionWithoutSkill]}
+    ],
+    [
+      [classQuestionWithSkill, javaQuestionWithSkill], 
+      {'Java': [classQuestionWithoutSkill, javaQuestionWithoutSkill]}
+    ],
+  ])('given questions %p, should group by skills as %p', (input, expectedResult) => {
+    expect(groupQuestionsBySkills(input)).toEqual(expectedResult);
+  });
+});
