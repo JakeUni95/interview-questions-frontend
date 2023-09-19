5d8da0091b1e2ed2ee7b05765d0c918fa0b63f12
JakeUni95 <jakelewiskirk@hotmail.com>
2023-09-19 16:42:43 +0100

    Reformatted slugbyname tests

--- helpers/cms/groupSlugsByName.test.js
+++ helpers/cms/groupSlugsByName.test.js
@@ -2,38 +2,36 @@ const groupSlugsByName = require('./groupSlugsByName');
 
 describe('groupSlugsByName', () => {
 
-  it('It should group slugs by their associated skill names', () => {
+  describe('Typical Usage', () => {
 
-    const emptyInput = [];
-    const emptyOutput = {};
-
-    const noSlugs = [{
-      skillName: 'Java'
-    }];
-
-    const noSlugsOutput = {
-      'Java': [undefined]
-    };
-
-  
-    const oneSlugOneName = [{ 
-      skillName: 'Java', 
-      slug: 'java-slug' 
-    }];
-
-    const oneSlugOneNameOutput = { 
-      Java: ['java-slug'] 
-    };
-
-    const emptyInputResult = groupSlugsByName(emptyInput);
-    expect(emptyInputResult).toEqual(emptyOutput);
-
-    const noSlugsResult = groupSlugsByName(noSlugs);
-    expect(noSlugsResult).toEqual(noSlugsOutput);
-
-    const oneSlugOneNameResult = groupSlugsByName(oneSlugOneName);
-    expect(oneSlugOneNameResult).toEqual(oneSlugOneNameOutput);
+    it.each([
+      [
+        [{ skillName: 'Java', slug: 'java-slug' }],
+        { 'Java': ['java-slug'] }
+      ],
+    ])('given slugs %p, should group by skill names as %p', (input, expectedResult) => {
+      expect(groupSlugsByName(input)).toEqual(expectedResult);
+    });
   });
+
+  describe('Edge Cases', () => {
+
+    it.each([
+      [
+        [], 
+        {}
+      ],
+      [  
+        [{ skillName: 'Java' }],
+        { 'Java': [undefined] }
+      ],
+      [
+        [{ skillName: '', slug: 'empty-slug'}],
+        { '': ['empty-slug'] }
+      ],
+    ])('given slugs %p, should group by skill names as %p', (input, expectedResult) => {
+      expect(groupSlugsByName(input)).toEqual(expectedResult);
+    });
+  });
+
 });
-
-
